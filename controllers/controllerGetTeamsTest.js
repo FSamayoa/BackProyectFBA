f3662ef6cd1e2b62b828dd5020a1f3ac5cc78a38
Samayoa <fsamayoal@gmail.com>
2024-03-15 18:21:53 -0600

    separados

--- controllers/controllerGetTeamsTest.js
+++ controllers/controllerGetTeamsTest.js
@@ -1,3 +1,17 @@
+const supabase = require("../supabase");
 
-require ("dotenv").config();
+async function getAllMatches(req, res) {
+  try {
+    const { data, error } = supabase.from('partidos').select('*');
+    if (error) {
+      console.error("error en supabase: ", error.message);
+      return res.status(500).json({ error: error.message });
+    }
+    res.json(data);
+  } catch {
+    console.error("Error al obtener equipos:", error.message);
+    res.status(500).json({ error: "Error interno del servidor" });
+  }
+}
 
+module.exports = getAllMatches
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,22 +1,9 @@
 const {Router} = require ("express")
+const getAllMatches = require('../../controllers/controllerGetTeamsTest')
 
 const routes = Router();
 
-const supabase = require("../../supabase")
-
-routes.get('/', async (req, res) => {
-    try {
-        const { data, error } = await supabase.from('equipos').select('*'); 
-        if (error) {
-            console.error('Error al obtener datos de Supabase:', error.message);
-            return res.status(500).json({ error: error.message });
-        }
-        res.json(data);
-    } catch (error) {
-        console.error('Error al obtene:', error.message);
-        res.status(500).json({ error: 'Error interno del servido dsfsdf' });
-    }
-});
-
+routes.get('/', getAllMatches)
+   
 
 module.exports = routes
