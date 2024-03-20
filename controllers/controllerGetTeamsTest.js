3d4fbe89c7dfb78d248c4c28858d5eb6575674fb
Samayoa <fsamayoal@gmail.com>
2024-03-19 19:12:09 -0600

    relaciones

--- controllers/controllerGetTeamsTest.js
+++ controllers/controllerGetTeamsTest.js
@@ -2,13 +2,22 @@ const supabase = require("../supabase");
 
 async function getAllMatches(req, res) {
   try {
-    const { data, error } = await supabase.from('partidos').select('*');
+
+    
+    // await supabase.cache.schema();
+
+    const { data, error } = await supabase
+    .from('partidos')
+    .select('*, equipos_locales (id, nom_equipo_local), equipos_visitantes (id, nom_equipo_visitante)');
+    
+
+
     if (error) {
         console.error('Error al obtener datos de Supabase:', error.message);
         return res.status(500).json({ error: error.message });
     }
     res.json(data);
-  } catch {
+  } catch (error){
     console.error("Error al obtener equipos:", error.message);
     res.status(500).json({ error: "Error interno del servidor" });
   }
